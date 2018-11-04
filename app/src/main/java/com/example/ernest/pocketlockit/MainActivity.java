4b54f2d545c84dbe9765af0fb201bc34d982f6eb
ParmarManpreet <manpreet.parmar29@hotmail.com>
2018-11-03 22:41:04 -0400

    Edit Button

--- app/src/main/java/com/example/ernest/pocketlockit/MainActivity.java
+++ app/src/main/java/com/example/ernest/pocketlockit/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -91,4 +93,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+
+        switch(id){
+            case R.id.menu:
+                Toast.makeText(this,"Edit Button Clicked",Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
 }
